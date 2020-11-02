91b295f303ed9db855b8f1a629e6aaab5cccc8ac
Vladimir <andiwillfly@gmail.com>
2020-11-02 13:28:13 +0200

    Add fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { observable } from "mobx";
+import { observer } from "mobx-react";
 
 
+@observer
 class App extends React.Component {
 
-    render() {
-        const mediaItemsInRow = 4;
 
-        let arr = [{w:1,h:1}, {w:1,h:1}, {w:1,h:1}, {w:1,h:1}, {w:1,h:1},  {w:1,h:1}];
-        const itemsInRow = arr.length < mediaItemsInRow ? arr.length : mediaItemsInRow;
+    @observable mediaItemsInRow = 4;
+    @observable arrLength = 12;
+
+
+    render() {
+        let arr = new Array(this.arrLength).fill({ w:1, h:1 });
+        const itemsInRow = arr.length < this.mediaItemsInRow ? arr.length : this.mediaItemsInRow;
         const emptySlots = Math.ceil(arr.length/itemsInRow) * itemsInRow - arr.length;
         let currX = { 0: 0 };
 
@@ -16,51 +22,60 @@ class App extends React.Component {
             h: emptySlots > i ? item.h+1 : item.h
         }))
 
-        console.log(emptySlots, 'emptySlots');
-        console.log(itemsInRow, 'itemsInRow');
+        let isFixedX = false;
 
         return (
-            <div style={{
-                margin: 5,
-                display: 'grid',
-                gridTemplateColumns: `repeat(${itemsInRow}, 1fr)`,
-                gridTemplateRows: `repeat(${itemsInRow}, 1fr)`,
-                gridColumnGap: '5px',
-                gridRowGap: '5px'
-            }}>
-                { arr.map(({ w, h }, i) => {
-                    let x = 0;
+            <div style={{ margin: 10 }}>
+                <h4>Items count: { arr.length }</h4>
+                <h4>Items in row: { itemsInRow }</h4>
+                <h4>Empty slots in the end of last row: { emptySlots }</h4>
 
-                    while(x < 10 && arr[(i+x)-itemsInRow]?.h === 2) {
-                        console.log('||||', x);
-                        x+=1;
-                    }
+                <hr/>
+                <input type="number"
+                       value={ this.arrLength }
+                       onChange={ (e)=> this.arrLength = +e.target.value || 1  } />
+                <label>arrLength</label>
 
-                    const isPrevHeight = arr[i-itemsInRow]?.h === 2;
+                <hr/>
+                <input type="number"
+                       value={ this.mediaItemsInRow }
+                       onChange={ (e)=> this.mediaItemsInRow = +e.target.value || 1 } />
+                <label>mediaItemsInRow</label>
+                <hr/>
+                <div style={{
+                    display: 'grid',
+                    gridTemplateColumns: `repeat(${itemsInRow}, 1fr)`,
+                    gridTemplateRows: `repeat(${itemsInRow}, 1fr)`,
+                    gridColumnGap: '5px',
+                    gridRowGap: '5px'
+                }}>
+                    { arr.map(({ w, h }, i) => {
+                        const isPrevHeight = arr[i-itemsInRow] && arr[i-itemsInRow].h === 2 && !isFixedX;
+                        let row = Math.ceil((i+1) / itemsInRow);
 
-                    const row = Math.ceil((i+1) / itemsInRow);
+                        if(currX[row] >= itemsInRow) row+=1;
 
-                    console.log(currX[row] || w, isPrevHeight, 'x:', x);
+                        currX[row] = currX[row] ? currX[row]+w : w;
+                        if(isPrevHeight) isFixedX = true;
+                        if(isPrevHeight) currX[row]+= emptySlots;
 
-                    currX[row] = currX[row] ? currX[row]+w : w;
-                    if(isPrevHeight) currX[row]+= emptySlots;
+                        const x1 = currX[row];
+                        const y1 = row;
+                        const x2 =  currX[row]+w;
+                        const y2 = row + h;
 
-                    const x1 = currX[row];
-                    const y1 = row;
-                    const x2 =  currX[row]+w;
-                    const y2 = row + h;
-
-                    return (
-                        <div key={i}
-                             style={{
-                                 background: h === 1 ? 'gray' : 'orange',
-                                 gridArea: `${y1} / ${x1} / ${y2} / ${x2}`
-                             }}>
-                            {i} | h:{h}
-                            {/*{ `x:${x1} y:${y1} / x:${x2} y:${y2}` }*/}
-                        </div>
-                    );
-                }) }
+                        return (
+                            <div key={i}
+                                 style={{
+                                     padding: 5,
+                                     background: h === 1 ? 'lightgray' : 'orange',
+                                     gridArea: `${y1} / ${x1} / ${y2} / ${x2}`
+                                 }}>
+                                {row} | y:{y1} | y:{y2}
+                            </div>
+                        );
+                    }) }
+                </div>
             </div>
         )
     }
