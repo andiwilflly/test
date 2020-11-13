a6d0c6b1e627be56cdc31a03d9f0082cb2ae7ca6
andiwillfly <andiwillfly@gmail.com>
2020-11-13 18:18:53 +0200

    deploy changes

--- src/components/_parts/offers/MainOffersSavingSlider.component.js
+++ src/components/_parts/offers/MainOffersSavingSlider.component.js
@@ -2,8 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 // MobX
 import { observer } from "mobx-react";
+// Styles
+import "src/styles/scoped/offers/MainOffersSavingSlider.scoped.scss";
 // Components
 import OffersSlider from "src/components/_parts/offers/OffersSlider.component";
+import { Desktop, Tablet, Mobile } from "src/components/_parts/MediaQuery.component";
 
 
 @observer
@@ -16,8 +19,27 @@ class MainOffersSavingSlider extends React.Component {
 
     render() {
         return (
-            <div className="offers">
-                <OffersSlider offers={ this.props.offers } slidesToShow={2} />
+            <div className="saving-offer">
+                <div className="saving-info">
+                    <div className="saving-title">С нами ты экономишь</div>
+
+                    <div className="saving-description">
+                        Asd ekwr jkwenf cksdnck sdkc msdcks dmkfner terg fd??
+                    </div>
+
+                    <button className="button:green">Смотреть все</button>
+                </div>
+                <div className="saving-offers">
+                    <Desktop>
+                        <OffersSlider offers={ this.props.offers } slidesToShow={2} />
+                    </Desktop>
+                    <Tablet>
+                        <OffersSlider offers={ this.props.offers } slidesToShow={1} />
+                    </Tablet>
+                    <Mobile>
+                        <OffersSlider offers={ this.props.offers } slidesToShow={1} />
+                    </Mobile>
+                </div>
             </div>
         );
     }
